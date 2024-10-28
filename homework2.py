19226eafde808b58e8b88efead4a3163d129a12b
XpomM96 <xokkeist69@gmail.com>
2024-10-29 00:16:03 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = ('Nikita')
+print(name, type(name))
+age = 28
+print(age, type(28))
+new_age = 1
+print(age + new_age, type(age + new_age))
+is_student = (5 == 5 )
+print(is_student, type(5 == 5))
